e705488d4be9ccfd687e6fc4034e495cd00f5887
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-04 17:40:08 +0800

    Practice with Variables

---
+++ 3-Variable.py
@@ -0,0 +1,21 @@
+
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multiline
+sentence'''
+print(s)
+
+print("The output is \
+{}" .format(i))
+
+length = 4
+width = 7
+
+area = 7 * 4
+perimeter = 2*(length + width)
+
+print("The Area is",area)
+print("The Perimeter is",perimeter)
