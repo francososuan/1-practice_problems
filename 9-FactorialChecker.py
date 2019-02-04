1c05e6f3ad4a10668b3fa943d5d125732c3e429b
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-04 17:50:14 +0800

    Sample code for Checking the number of trailing zeros in factorial

---
+++ 9-FactorialChecker.py
@@ -0,0 +1,11 @@
+Factorial = int(input("Please input Factorial: "))
+counter = 0
+i = 0
+
+while Factorial // 5 != 0:
+    Factorial = Factorial //5
+    counter = counter + Factorial
+
+
+print("Number of 0 are: {}".format(counter))
+
