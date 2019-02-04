0bc8a8195870a33df007ee9518b5d0337dce15e8
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-04 13:17:15 +0800

    1st version test

---
+++ 2-String.py
@@ -0,0 +1,24 @@
+age = 22
+name = "Franco Sosuan"
+
+print("My name is {0}".format(name))
+
+print("My name is {0} and my age is {1}".format(name,age))
+
+print("Programming is my {g}" .format(g = "goal"))
+
+fraction = 1/3
+
+print("The fraction is {0:.5f}".format(fraction))
+
+print('Spacing {0: ^11}' .format("hello"))
+
+print("a", end = " ")
+print("b" ,end = "  \n" )
+
+print("What\'s your name?")
+
+print (" What is your\
+ name"   )
+
+print(r"Hi//Adfa\\n \t")
